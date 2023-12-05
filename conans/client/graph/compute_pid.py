354a911fc59f04299b97a3dc406c7fc4579d0c22
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-04 21:48:49 -0500

    spelling: been
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- conans/client/graph/compute_pid.py
+++ conans/client/graph/compute_pid.py
@@ -64,7 +64,7 @@ def compute_package_id(node, new_config):
     run_validate_package_id(conanfile)
 
     if conanfile.info.settings_target:
-        # settings_target has beed added to conan package via package_id api
+        # settings_target has been added to conan package via package_id api
         conanfile.original_info.settings_target = conanfile.info.settings_target
 
     info = conanfile.info
